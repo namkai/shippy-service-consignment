ecb71728f0f9dc9036e0b2d73e36592f8e056781
namkaif <namkai.fairfield@hired.com>
2020-01-24 16:43:13 -0800

    Breakup code and add docker-compose file

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/namkai/shippy-service-consignment
 go 1.13
 
 require (
+	github.com/DataDog/zstd v1.4.4 // indirect
 	github.com/golang/protobuf v1.3.2
 	github.com/micro/go-micro v1.18.0
-	golang.org/x/net v0.0.0-20191109021931-daa7c04131f5
+	github.com/namkai/shippy-service-vessel v0.0.0-20200124204505-7dff59c7078a
+	github.com/pkg/errors v0.8.1
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.2.1
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa
 )
--- main.go
+++ main.go
@@ -3,80 +3,44 @@ package main
 import (
 	"context"
 	"fmt"
-
 	"github.com/micro/go-micro"
 	pb "github.com/namkai/shippy-service-consignment/proto/consignment"
+	vesselProto "github.com/namkai/shippy-service-vessel/proto/vessel"
+	"log"
+	"os"
 )
 
-type repository interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
-}
-
-// Repository - Dummy repository, this simulates the use of a datastore
-// of some kind. We'll replace this with a real implementation later on.
-type Repository struct {
-	consignments []*pb.Consignment
-}
-
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	updated := append(repo.consignments, consignment)
-	repo.consignments = updated
-	return consignment, nil
-}
-
-func (repo *Repository) GetAll() []*pb.Consignment {
-	return repo.consignments
-}
-
-// Service should implement all of the methods to satisfy the service
-// we defined in our protobuf definition. You can check the interface
-// in the generated code itself for the exact method signatures etc
-// to give you a better idea.
-type service struct {
-	repo repository
-}
-
-// CreateConsignment - we created just one method on our service,
-// which is a create method, which takes a context and a request as an
-// argument, these are handled by the gRPC server.
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
-
-	// Save our consignment
-	consignment, err := s.repo.Create(req)
-	if err != nil {
-		return err
-	}
-
-	// Return matching the `Response` message we created in our
-	// protobuf definition.
-	res.Created = true
-	res.Consignment = consignment
-	return nil
-}
-
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
-	res.Consignments = consignments
-	return nil
-}
+const (
+	defaultHost = "datastore:27017"
+)
 
 func main() {
-
-	repo := &Repository{}
-
-	// Create a new service. Optionally include some options here.
+	// Set-up micro instance
 	srv := micro.NewService(
-
-		// This name must match the package name given in your protobuf definition
 		micro.Name("shippy.service.consignment"),
 	)
 
-	// Init will parse the command line flags.
 	srv.Init()
 
-	// Register handler
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	uri := os.Getenv("DB_HOST")
+	if uri == "" {
+		uri = defaultHost
+	}
+
+	client, err := CreateClient(context.Background(), uri, 0)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer client.Disconnect(context.Background())
+
+	consignmentCollection := client.Database("shippy").Collection("consignments")
+
+	repository := &MongoRepository{consignmentCollection}
+	vesselClient := vesselProto.NewVesselServiceClient("shippy.service.client", srv.Client())
+	h := &handler{repository, vesselClient}
+
+	// Register handlers
+	pb.RegisterShippingServiceHandler(srv.Server(), h)
 
 	// Run the server
 	if err := srv.Run(); err != nil {
